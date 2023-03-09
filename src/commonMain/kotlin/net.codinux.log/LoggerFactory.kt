bde2406d1ec929a0af9645510d98a0dd2b35a512
dankito <git@dankito.net>
2023-03-09 23:16:00 +0100

    Made LoggerFactory methods static on the JVM so that they can be called with LoggerFactory.xyz()

--- src/commonMain/kotlin/net.codinux.log/LoggerFactory.kt
+++ src/commonMain/kotlin/net.codinux.log/LoggerFactory.kt
@@ -1,5 +1,6 @@
 package net.codinux.log
 
+import kotlin.jvm.JvmStatic
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty
@@ -8,6 +9,7 @@ object LoggerFactory {
 
     private var factory: ILoggerFactory = DefaultLoggerFactory().createDefaultLoggerFactory()
 
+    @JvmStatic
     fun setLoggerFactory(factory: ILoggerFactory) {
         this.factory = factory
     }
@@ -15,10 +17,12 @@ object LoggerFactory {
 
     inline fun <reified R : Any> logger() = LoggerDelegate<R>()
 
+    @JvmStatic
     fun getLogger(name: String): Logger {
         return factory.getLogger(name)
     }
 
+    @JvmStatic
     fun getLogger(forClass: KClass<*>): Logger =
         getLogger(getLoggerName(forClass))
 
