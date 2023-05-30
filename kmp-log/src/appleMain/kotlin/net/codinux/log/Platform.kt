b64849dce3152c5b4456175a3a672dd60d0ddbdc
dankito <git@dankito.net>
2023-05-30 22:01:29 +0200

    Implemented determining if os_log is supported on platform

--- kmp-log/src/appleMain/kotlin/net/codinux/log/Platform.kt
+++ kmp-log/src/appleMain/kotlin/net/codinux/log/Platform.kt
@@ -1,19 +1,43 @@
 package net.codinux.log
 
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.cinterop.useContents
 import net.codinux.log.appender.Appender
 import net.codinux.log.appender.NSLogAppender
+import net.codinux.log.oslog.OSLogAppender
+import platform.Foundation.NSProcessInfo
+import kotlin.native.Platform
 import kotlin.reflect.KClass
 
 actual class Platform {
 
   actual companion object {
 
+    private val supportsOsLog = isOsLogSupported()
+
     actual fun createDefaultLoggerFactory(): ILoggerFactory = DefaultLoggerFactory()
 
-    actual val systemDefaultAppender: Appender = NSLogAppender()
+    actual val systemDefaultAppender: Appender = if (supportsOsLog) OSLogAppender() else NSLogAppender()
 
     actual fun <T : Any> getLoggerName(forClass: KClass<T>) =
       forClass.qualifiedName ?: forClass.simpleName ?: forClass.toString().replace("class ", "")
+
+
+    @OptIn(UnsafeNumber::class)
+    private fun isOsLogSupported(): Boolean {
+      val osVersion = NSProcessInfo.processInfo.operatingSystemVersion
+
+      return osVersion.useContents {
+        val majorVersion = this.majorVersion
+
+        // os_log is available for: iOS 10.0+, iPadOS 10.0+, macOS 10.12+, Mac Catalyst 13.1+, tvOS 10.0+, watchOS 3.0+ (https://developer.apple.com/documentation/os/1643744-os_log_create)
+        when (Platform.osFamily) {
+          OsFamily.IOS, OsFamily.MACOSX, OsFamily.TVOS -> majorVersion >= 10
+          OsFamily.WATCHOS -> majorVersion >= 3
+          else -> false
+        }
+      }
+    }
 
   }
 
