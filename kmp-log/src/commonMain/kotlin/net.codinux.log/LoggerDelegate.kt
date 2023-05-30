cb580dd0dd506f1e82f3fbd33e079128966bc028
dankito <git@dankito.net>
2023-05-30 23:57:20 +0200

    Implemented logger delegate for loggerName / String

--- kmp-log/src/commonMain/kotlin/net.codinux.log/LoggerDelegate.kt
+++ kmp-log/src/commonMain/kotlin/net.codinux.log/LoggerDelegate.kt
@@ -4,8 +4,19 @@ import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
 
+// by class
+
 inline fun <reified R : Any> logger() = LoggerDelegate<R>()
 
 class LoggerDelegate<in R : Any> : ReadOnlyProperty<R, Logger> {
   override fun getValue(thisRef: R, property: KProperty<*>) = LoggerFactory.getLogger(thisRef::class)
+}
+
+
+// by String / loggerName
+
+fun logger(loggerName: String) = LoggerDelegateForName(loggerName)
+
+class LoggerDelegateForName(val loggerName: String) {
+  inline operator fun getValue(thisRef: Any, property: KProperty<*>) = LoggerFactory.getLogger(loggerName)
 }
--- sampleApps/NativeSampleApp/src/nativeMain/kotlin/net/codinux/log/NativeSampleApp.kt
+++ sampleApps/NativeSampleApp/src/nativeMain/kotlin/net/codinux/log/NativeSampleApp.kt
@@ -4,6 +4,9 @@ class NativeSampleApp {
 
     private val log by logger()
 
+    private val logByName by logger("Liebe")
+
+
     fun runSample() {
         log.debug { "Debug" }
 
@@ -12,5 +15,7 @@ class NativeSampleApp {
         log.warn { "Warn" }
 
         log.error(Exception("Just a test, no animals have been harmed")) { "Error with Exception" }
+
+        logByName.info { "Info from (lazy instantiated) Logger by name" }
     }
 }
