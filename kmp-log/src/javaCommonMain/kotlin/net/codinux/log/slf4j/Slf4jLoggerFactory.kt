2ea08eb696fec86d78b84a74ec67a3b0896a57eb
dankito <git@dankito.net>
2023-06-03 23:30:46 +0200

    Removed adding slf4j appenders to our internal appenders list as it's not used anywhere

--- kmp-log/src/javaCommonMain/kotlin/net/codinux/log/slf4j/Slf4jLoggerFactory.kt
+++ kmp-log/src/javaCommonMain/kotlin/net/codinux/log/slf4j/Slf4jLoggerFactory.kt
@@ -13,13 +13,6 @@ import java.util.logging.LogManager
 
 open class Slf4jLoggerFactory : LoggerFactoryBase() {
 
-    protected open val isSupportedLoggingFramework: Boolean by lazy {
-        when (Slf4jUtil.boundLoggingFramework) {
-            Slf4jBinding.Logback, Slf4jBinding.Log4j2, Slf4jBinding.JUL -> true
-            else -> false
-        }
-    }
-
     protected open val log4j2Configurator: Log4j2Configurator by lazy { Log4j2Configurator() }
 
 
@@ -35,28 +28,15 @@ open class Slf4jLoggerFactory : LoggerFactoryBase() {
 
     override fun addAppender(appender: Appender) {
         when (Slf4jUtil.boundLoggingFramework) {
-            Slf4jBinding.Logback -> {
-                (Slf4jUtil.slf4jLoggerFactory as LoggerContext).getLogger(Logger.ROOT_LOGGER_NAME).addAppender(LogbackAppender(appender))
-                super.addAppender(appender)
-            }
-            Slf4jBinding.Log4j2 -> {
-                log4j2Configurator.addAppender(Log4j2Appender(appender))
-                super.addAppender(appender)
-            }
-            Slf4jBinding.JUL -> {
-                LogManager.getLogManager().getLogger("").addHandler(JavaUtilLoggingAppender(appender)) // the root logger has name ""
-                super.addAppender(appender)
-            }
-            else -> System.err.println("Logging is delegated to slf4j. Configure appenders via the logging backend (log4j, Java Util Log, Slf4jSimple, ...), appenders added to KMP-Log will be ignored.")
+            Slf4jBinding.Logback -> (Slf4jUtil.slf4jLoggerFactory as LoggerContext).getLogger(Logger.ROOT_LOGGER_NAME).addAppender(LogbackAppender(appender))
+            Slf4jBinding.Log4j2 -> log4j2Configurator.addAppender(Log4j2Appender(appender))
+            Slf4jBinding.JUL -> LogManager.getLogManager().getLogger("").addHandler(JavaUtilLoggingAppender(appender)) // the root logger has name ""
+            else -> System.err.println("Logging is delegated to slf4j. Configure appenders via the logging backend (Slf4jSimple, ...), appenders added to KMP-Log will be ignored.")
         }
     }
 
     override fun getAppenders(): Collection<Appender> {
-        if (isSupportedLoggingFramework) {
-            return super.getAppenders()
-        }
-
-        throw IllegalStateException("Logging is delegated to slf4j. So use the appenders of the logging backend (log4j, Java Util Log, Slf4jSimple, ...), not KMP-Log appenders.")
+        throw IllegalStateException("Logging is delegated to slf4j. So use the appenders of the logging backend (logback, log4j, Java Util Log, Slf4jSimple, ...), not KMP-Log appenders.")
     }
 
 }
