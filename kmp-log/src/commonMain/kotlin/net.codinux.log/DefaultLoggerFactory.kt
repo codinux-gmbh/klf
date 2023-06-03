2f12948d4844b83f07cea6fec6ee013fd4add2cc
dankito <git@dankito.net>
2023-06-03 21:35:08 +0200

    Fixed that systemDefaultAppender hasn't been added anymore

--- kmp-log/src/commonMain/kotlin/net.codinux.log/DefaultLoggerFactory.kt
+++ kmp-log/src/commonMain/kotlin/net.codinux.log/DefaultLoggerFactory.kt
@@ -2,6 +2,11 @@ package net.codinux.log
 
 open class DefaultLoggerFactory : LoggerFactoryBase() {
 
+  init {
+    addAppender(Platform.systemDefaultAppender)
+  }
+
+
   override fun createLogger(name: String) =
     DelegateToAppenderLogger(name, this)
 
