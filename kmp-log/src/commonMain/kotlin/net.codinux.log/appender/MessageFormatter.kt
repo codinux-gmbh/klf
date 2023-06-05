7dabe95185bacd8eb28101ff946d187315bf2afb
dankito <git@dankito.net>
2023-06-05 18:34:23 +0200

    Added TODO / suggestion

--- kmp-log/src/commonMain/kotlin/net.codinux.log/appender/MessageFormatter.kt
+++ kmp-log/src/commonMain/kotlin/net.codinux.log/appender/MessageFormatter.kt
@@ -13,6 +13,14 @@ open class MessageFormatter {
     }
 
     open fun formatMessage(level: LogLevel, message: String, loggerName: String, threadName: String? = null, exception: Throwable? = null): String {
+        // may add format specifiers as logback has:
+        // %d{yyyy-MM-dd HH:mm:ss,SSS} %-5p [%c{3.}] (%t) %s%e%n
+        // p = level
+        // c = category name (= logger name)
+        // t = thread name
+        // s = Simple message (Renders just the log message, with no exception trace)
+        // e = Exception
+        // n = Newline
         return "${threadName?.let { "[$it] " } ?: ""}$level $loggerName - ${formatMessage(message, exception)}"
     }
 
