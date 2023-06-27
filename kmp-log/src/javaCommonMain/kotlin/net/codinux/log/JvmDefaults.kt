6d0ea4452172d362e75dcb82bdb853bba6e0ff32
dankito <git@dankito.net>
2023-06-27 18:53:47 +0200

    Fixed max log tag length on older Androids

--- kmp-log/src/javaCommonMain/kotlin/net/codinux/log/JvmDefaults.kt
+++ kmp-log/src/javaCommonMain/kotlin/net/codinux/log/JvmDefaults.kt
@@ -3,6 +3,7 @@ package net.codinux.log
 import net.codinux.log.slf4j.Slf4jLoggerFactory
 import net.codinux.log.slf4j.Slf4jUtil
 import kotlin.reflect.KClass
+import kotlin.reflect.jvm.jvmName
 
 object JvmDefaults {
 
@@ -15,15 +16,22 @@ object JvmDefaults {
   }
 
 
-  fun <T : Any> getLoggerName(forClass: KClass<T>) = getLoggerName(forClass.java)
+  fun <T : Any> getLoggerName(forClass: KClass<T>) = getLoggerClass(forClass).jvmName
 
-  fun <T : Any> getLoggerName(forClass: Class<T>): String = unwrapCompanionClass(forClass).name
+  fun <T : Any> getLoggerName(forClass: Class<T>): String = getLoggerName(forClass.kotlin)
+
+  fun <T : Any> getLoggerClass(ofClass: KClass<T>): KClass<*> {
+    return unwrapCompanionClass(ofClass)
+  }
 
   // unwrap companion class to enclosing class given a Java Class
-  private fun <T : Any> unwrapCompanionClass(ofClass: Class<T>): Class<*> {
-    return ofClass.enclosingClass?.takeIf {
-      ofClass.kotlin.isCompanion
-    } ?: ofClass
+  private fun <T : Any> unwrapCompanionClass(ofClass: KClass<T>): KClass<*> {
+    return if (ofClass.isCompanion) {
+      ofClass.java.enclosingClass?.kotlin // enclosingClass should never be null, just in case
+        ?: ofClass
+    } else {
+      ofClass
+    }
   }
 
 
