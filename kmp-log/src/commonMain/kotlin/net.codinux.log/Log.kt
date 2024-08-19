a8673197ec59fdf5bcd6bf020cdf29d60a32d57f
dankito <git@dankito.net>
2024-08-19 02:30:08 +0200

    Fixed getting logger when loggerClass is specified as otherwise this will crash in native image

--- kmp-log/src/commonMain/kotlin/net.codinux.log/Log.kt
+++ kmp-log/src/commonMain/kotlin/net.codinux.log/Log.kt
@@ -8,34 +8,34 @@ object Log {
         LoggerFactory.getLogger(loggerName).trace(exception) { messageSupplier() }
 
     inline fun <reified T : Any> trace(exception: Throwable? = null, loggerClass: KClass<T> = T::class, crossinline messageSupplier: () -> String) =
-        trace(exception, Platform.getLoggerName(loggerClass), messageSupplier)
+        LoggerFactory.getLogger(loggerClass).trace(exception) { messageSupplier() }
 
 
     inline fun debug(exception: Throwable? = null, loggerName: String? = null, crossinline messageSupplier: () -> String) =
         LoggerFactory.getLogger(loggerName).debug(exception) { messageSupplier() }
 
     inline fun <reified T : Any> debug(exception: Throwable? = null, loggerClass: KClass<T> = T::class, crossinline messageSupplier: () -> String) =
-        debug(exception, Platform.getLoggerName(loggerClass), messageSupplier)
+        LoggerFactory.getLogger(loggerClass).debug(exception) { messageSupplier() }
 
 
     inline fun info(exception: Throwable? = null, loggerName: String? = null, crossinline messageSupplier: () -> String) =
         LoggerFactory.getLogger(loggerName).info(exception) { messageSupplier() }
 
     inline fun <reified T : Any> info(exception: Throwable? = null, loggerClass: KClass<T> = T::class, crossinline messageSupplier: () -> String) =
-        info(exception, Platform.getLoggerName(loggerClass), messageSupplier)
+        LoggerFactory.getLogger(loggerClass).info(exception) { messageSupplier() }
 
 
     inline fun warn(exception: Throwable? = null, loggerName: String? = null, crossinline messageSupplier: () -> String) =
         LoggerFactory.getLogger(loggerName).warn(exception) { messageSupplier() }
 
     inline fun <reified T : Any> warn(exception: Throwable? = null, loggerClass: KClass<T> = T::class, crossinline messageSupplier: () -> String) =
-        warn(exception, Platform.getLoggerName(loggerClass), messageSupplier)
+        LoggerFactory.getLogger(loggerClass).warn(exception) { messageSupplier() }
 
 
     inline fun error(exception: Throwable? = null, loggerName: String? = null, crossinline messageSupplier: () -> String) =
         LoggerFactory.getLogger(loggerName).error(exception) { messageSupplier() }
 
     inline fun <reified T : Any> error(exception: Throwable? = null, loggerClass: KClass<T> = T::class, crossinline messageSupplier: () -> String) =
-        error(exception, Platform.getLoggerName(loggerClass), messageSupplier)
+        LoggerFactory.getLogger(loggerClass).error(exception) { messageSupplier() }
 
 }
