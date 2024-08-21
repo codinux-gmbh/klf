6e6894d9348f966acd3cb55d648b7a6f02e2eaf0
dankito <git@dankito.net>
2024-08-22 00:28:30 +0200

    Bumped version to 2.0.0-SNAPSHOT

--- sampleApps/QuarkusSampleApp/build.gradle.kts
+++ sampleApps/QuarkusSampleApp/build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
 
     implementation("io.quarkus:quarkus-smallrye-openapi")
 
-    implementation("net.codinux.log:klf-graal:1.5.1")
+    implementation("net.codinux.log:klf-graal:2.0.0-SNAPSHOT")
 
 
     testImplementation("io.quarkus:quarkus-junit5")
