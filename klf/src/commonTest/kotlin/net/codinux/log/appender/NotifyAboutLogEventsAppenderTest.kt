43dbc5bff3503a92119aa2c6a6a528be7f08bb8d
dankito <git@dankito.net>
2024-12-11 23:46:02 +0100

    Fixed test for all platforms (loggerName was different)

--- klf/src/commonTest/kotlin/net/codinux/log/appender/NotifyAboutLogEventsAppenderTest.kt
+++ klf/src/commonTest/kotlin/net/codinux/log/appender/NotifyAboutLogEventsAppenderTest.kt
@@ -12,17 +12,23 @@ import kotlin.test.Test
 
 class NotifyAboutLogEventsAppenderTest {
 
+    companion object {
+        private val loggerName = "main"
+
+        private val exceptionMessage = "Just a test"
+    }
+
     @Test
     fun append_InfoWithoutThreadName() {
         val receivedEvents = mutableListOf<LogEvent>()
         LoggerFactory.addAppender(NotifyAboutLogEventsAppender { receivedEvents.add(it) })
 
-        Log.info { "Info" }
+        Log.info(loggerName = loggerName) { "Info" }
 
         assertThat(receivedEvents).hasSize(1)
         assertThat(receivedEvents.first().level).isEqualTo(LogLevel.Info)
         assertThat(receivedEvents.first().message).isEqualTo("Info")
-        assertThat(receivedEvents.first().loggerName).isEqualTo(LoggerFactory.defaultLoggerName)
+        assertThat(receivedEvents.first().loggerName).isEqualTo(loggerName)
         assertThat(receivedEvents.first().threadName).isNull()
         assertThat(receivedEvents.first().exception).isNull()
     }
@@ -32,12 +38,12 @@ class NotifyAboutLogEventsAppenderTest {
         val receivedEvents = mutableListOf<LogEvent>()
         LoggerFactory.addAppender(NotifyAboutLogEventsAppender(true, false) { receivedEvents.add(it) })
 
-        Log.warn(Exception("Just a test")) { "Warn" }
+        Log.warn(Exception(exceptionMessage), loggerName) { "Warn" }
 
         assertThat(receivedEvents).hasSize(1)
         assertThat(receivedEvents.first().level).isEqualTo(LogLevel.Warn)
         assertThat(receivedEvents.first().message).isEqualTo("Warn")
-        assertThat(receivedEvents.first().loggerName).isEqualTo(LoggerFactory.defaultLoggerName)
+        assertThat(receivedEvents.first().loggerName).isEqualTo(loggerName)
         assertThat(receivedEvents.first().threadName).isNotNull()
         assertThat(receivedEvents.first().exception).isNull()
     }
@@ -47,15 +53,15 @@ class NotifyAboutLogEventsAppenderTest {
         val receivedEvents = mutableListOf<LogEvent>()
         LoggerFactory.addAppender(NotifyAboutLogEventsAppender(true) { receivedEvents.add(it) })
 
-        Log.error(Exception("Just a test")) { "Error" }
+        Log.error(Exception(exceptionMessage), loggerName) { "Error" }
 
         assertThat(receivedEvents).hasSize(1)
         assertThat(receivedEvents.first().level).isEqualTo(LogLevel.Error)
         assertThat(receivedEvents.first().message).isEqualTo("Error")
-        assertThat(receivedEvents.first().loggerName).isEqualTo(LoggerFactory.defaultLoggerName)
+        assertThat(receivedEvents.first().loggerName).isEqualTo(loggerName)
         assertThat(receivedEvents.first().threadName).isNotNull()
         assertThat(receivedEvents.first().exception).isNotNull()
-        assertThat(receivedEvents.first().exception!!.message).isEqualTo("Just a test")
+        assertThat(receivedEvents.first().exception!!.message).isEqualTo(exceptionMessage)
     }
 
 }
