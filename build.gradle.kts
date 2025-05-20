eccc1c929c33eb2f2412df96625843983bfc16aa
dankito <git@dankito.net>
2025-05-20 05:04:01 +0200

    Published version 1.8.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ buildscript {
 
 allprojects {
     group = "net.codinux.log"
-    version = "1.8.2-SNAPSHOT"
+    version = "1.8.2"
 
     ext["projectDescription"] = "Kotlin (Multiplatform) logging facade for idiomatic logging in Kotlin"
     ext["sourceCodeRepositoryBaseUrl"] = "github.com/codinux-gmbh/klf"
