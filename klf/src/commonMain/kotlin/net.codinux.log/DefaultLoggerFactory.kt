e43221d937236e1455f776c82f8cb8f408d9f2ed
dankito <git@dankito.net>
2025-06-01 17:09:20 +0200

    Extracted constant RootLoggerName

--- klf/src/commonMain/kotlin/net.codinux.log/DefaultLoggerFactory.kt
+++ klf/src/commonMain/kotlin/net.codinux.log/DefaultLoggerFactory.kt
@@ -2,12 +2,17 @@ package net.codinux.log
 
 open class DefaultLoggerFactory : LoggerFactoryBase() {
 
+  companion object {
+    const val RootLoggerName = ""
+  }
+
+
   init {
     addAppender(Platform.systemDefaultAppender)
   }
 
 
-  override val rootLogger = DelegateToAppendersLogger("", this, LoggerFactory.effectiveConfig.rootLevel)
+  override val rootLogger = DelegateToAppendersLogger(RootLoggerName, this, LoggerFactory.effectiveConfig.rootLevel)
 
   override fun createLogger(name: String) =
     DelegateToAppendersLogger(name, this)
