e45e088680a999dd4a20f8494c493883069cb9d7
dankito <git@dankito.net>
2025-06-02 14:18:00 +0200

    Prepared calling ErrorLogger

--- klf/src/androidMain/kotlin/net/codinux/log/Platform.android.kt
+++ klf/src/androidMain/kotlin/net/codinux/log/Platform.android.kt
@@ -6,6 +6,9 @@ import net.codinux.log.appender.LogcatAppender
 
 internal actual object Platform {
 
+    private var hasLoggedThatAppNameCannotBeRetrievedAsApplicationContextIsNotSet = false
+
+
     actual fun createDefaultLoggerFactory() = JvmDefaults.createDefaultLoggerFactory()
 
     actual val systemDefaultAppender: Appender by lazy { LogcatAppender.Default }
@@ -15,12 +18,22 @@ internal actual object Platform {
 
     actual val appName: String? by lazy {
       try {
-          AndroidContext.getApplicationContextIfInitialized()?.let { context ->
+          val context = AndroidContext.getApplicationContextIfInitialized()
+
+          if (context != null) {
               context.applicationInfo?.let { applicationInfo ->
                   applicationInfo.loadLabel(context.packageManager)?.let { label ->
                       "${applicationInfo.packageName ?: applicationInfo.processName}.$label"
                   }
               }
+          } else {
+              if (hasLoggedThatAppNameCannotBeRetrievedAsApplicationContextIsNotSet == false) {
+                  hasLoggedThatAppNameCannotBeRetrievedAsApplicationContextIsNotSet = true
+
+                  // TODO: call ErrorLogger
+              }
+
+              null
           }
       } catch (e: Throwable) {
           Log.error<Platform>(e) { "Could not get app name" }
