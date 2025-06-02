95d68c3684b63f1f7e8e52cdac9125dfb486ad4c
dankito <git@dankito.net>
2025-06-02 13:31:36 +0200

    Made immutable

--- klf/src/commonMain/kotlin/net.codinux.log/appender/Appender.kt
+++ klf/src/commonMain/kotlin/net.codinux.log/appender/Appender.kt
@@ -1,5 +1,6 @@
 package net.codinux.log.appender
 
+import net.codinux.collections.immutableSetOf
 import net.codinux.log.LogEvent
 import net.codinux.log.LogField
 
@@ -7,7 +8,7 @@ import net.codinux.log.LogField
 interface Appender {
 
   companion object {
-    val MinLoggedFields = setOf(LogField.Message, LogField.Level, LogField.LoggerName) // TODO: make immutable
+    val MinLoggedFields = immutableSetOf(LogField.Message, LogField.Level, LogField.LoggerName)
     val MinLoggedFieldsAndException = MinLoggedFields + LogField.Exception
     val MinLoggedFieldsAndExceptionAndThreadName = MinLoggedFieldsAndException + LogField.ThreadName
   }
